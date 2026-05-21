58c067088f3acd4e4a25e781c24a093950be8e41
arvietabz <23325391+arvietabz@users.noreply.github.com>
2026-05-21 08:08:25 +0800

    Create build.gradle.kts

---
+++ AnimeNexusProvider/build.gradle.kts
@@ -0,0 +1,18 @@
+cloudstream {
+    language = "en"
+    // All of the following are OPTIONAL, only include what's relevant
+    description = "Anime Nexus provider for Cloudstream"
+    authors = listOf("arvietabz")
+
+    /**
+     * Status int as follows:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1
+    tvTypes = listOf("Anime", "AnimeMovie", "OVA")
+
+    iconUrl = "https://anime.nexus/favicon.ico"
+}
